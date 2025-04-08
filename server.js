04a553ba4b417ff7b1c57fb8da831b46a5257ad8
lulu-67 <officialluluqiu@gmail.com>
2025-04-08 12:36:52 -0600

    0.0.0.0:3000

---
+++ server.js
@@ -0,0 +1,21 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = '0.0.0.0';
+const port = 3000;
+
+// Initialize Next.js
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, hostname, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+}); 
